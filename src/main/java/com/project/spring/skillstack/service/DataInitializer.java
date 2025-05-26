0fe2f60a827357221f1480d84898644d6694d5b5
민돌 <cheongmin0@gmail.com>
2025-05-26 19:34:04 +0900

    유저 초기 데이터 추가

--- src/main/java/com/project/spring/skillstack/service/DataInitializer.java
+++ src/main/java/com/project/spring/skillstack/service/DataInitializer.java
@@ -38,6 +38,56 @@ public class DataInitializer implements CommandLineRunner {
         userRep.save(root);
         userRep.save(abcd);
         
+
+        // 유저 초기화 데이터
+        
+        List<UserEntity> userList = new ArrayList<>();
+
+        for (int i = 1; i <= 20; i++) {
+            String suffix = String.format("%02d", i);
+            UserEntity user = new UserEntity(
+                null,
+                "user" + suffix,
+                passwordEncoder.encode("1234"),
+                "User " + suffix,
+                List.of("USER"),
+                "user" + suffix + "@pawple.com",
+                "0101234" + String.format("%04d", i),
+                null,
+                LocalDateTime.now(),
+                null,
+                new ArrayList<>()
+            );
+
+            PetEntity pet1 = new PetEntity(
+                "강아지",
+                5.0 + i % 3,
+                "댕댕이" + suffix,
+                1 + i % 5,
+                (i % 2 == 0) ? "수컷" : "암컷",
+                "푸들",
+                LocalDate.now().minusYears(1 + i % 3),
+                user
+            );
+
+            PetEntity pet2 = new PetEntity(
+                "고양이",
+                4.0 + i % 2,
+                "냐옹이" + suffix,
+                2 + i % 4,
+                (i % 2 == 0) ? "암컷" : "수컷",
+                "러시안블루",
+                LocalDate.now().minusYears(2 + i % 2),
+                user
+            );
+
+            user.getPets().add(pet1);
+            user.getPets().add(pet2);
+            userList.add(user);
+        }
+
+        userRep.saveAll(userList);
+
         // userRep.save(new UserEntity(null, "root", passwordEncoder.encode("1234"), "root", List.of("ADMIN"), null, null, null, LocalDateTime.now(), null, null));
         // userRep.save(new UserEntity(null, "abcd", passwordEncoder.encode("1234"), "abcd", List.of("USER"), null, null, null, LocalDateTime.now(), null, null));
     }
