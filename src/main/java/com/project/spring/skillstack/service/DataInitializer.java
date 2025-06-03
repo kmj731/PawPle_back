a687ff17144521b67f354427cdb39e6673005028
민돌 <cheongmin0@gmail.com>
2025-06-03 20:19:37 +0900

    초기화 데이터 수정

--- src/main/java/com/project/spring/skillstack/service/DataInitializer.java
+++ src/main/java/com/project/spring/skillstack/service/DataInitializer.java
@@ -37,10 +37,12 @@ public class DataInitializer implements CommandLineRunner {
 
         List<String> roles = new ArrayList<>();
         roles.add("USER");
+        List<String> roles2 = new ArrayList<>();
+        roles2.add("VET");
 
         UserEntity root = new UserEntity(null, "root", passwordEncoder.encode("1234"), "root", List.of("ADMIN"), null, "010-0000-0000", LocalDate.of(1999,9,9), null, null, LocalDateTime.now(), null, new ArrayList<>());
         UserEntity abcd = new UserEntity(null, "abcd", passwordEncoder.encode("1234"), "abcd", roles, "abc123@pawple.com", "010-1234-5678", null, null, null, LocalDateTime.now(), null, new ArrayList<>());
-        UserEntity vet = new UserEntity(null, "vet", passwordEncoder.encode("1234"), "vet", roles, "vet123@pawple.com", "010-4321-8765", null, null, null, LocalDateTime.now(), null, new ArrayList<>());
+        UserEntity vet = new UserEntity(null, "vet", passwordEncoder.encode("1234"), "vet", roles2, "vet123@pawple.com", "010-4321-8765", null, null, null, LocalDateTime.now(), null, new ArrayList<>());
 
         PetEntity abcdPet = new PetEntity("고양이", 4.0, "나비", 2025, "수컷", "코숏", LocalDate.now(), abcd);
         abcd.getPets().add(abcdPet);
