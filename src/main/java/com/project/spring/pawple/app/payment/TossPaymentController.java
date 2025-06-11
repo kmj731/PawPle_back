f05019083a4310b22fa929fa8eda1269cd64f9a1
sohyuneezz <csh8838@gmail.com>
2025-06-11 21:05:05 +0900

    주석 수정

--- src/main/java/com/project/spring/pawple/app/payment/TossPaymentController.java
+++ src/main/java/com/project/spring/pawple/app/payment/TossPaymentController.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,31 +27,6 @@ public class TossPaymentController {
     private String tossSecretKey;
     @Value("${spring.security.cors.site}")
     String corsOrigin;
-
-    // @PostMapping("/pay")
-    // public ResponseEntity<String> requestPayment(@RequestBody TossPaymentRequestDto dto) {
-    //     String basicAuth = Base64.getEncoder().encodeToString((tossSecretKey + ":").getBytes());
-
-    //     RestTemplate restTemplate = new RestTemplate();
-
-    //     HttpHeaders headers = new HttpHeaders();
-    //     headers.set("Authorization", "Basic " + basicAuth);
-    //     headers.setContentType(MediaType.APPLICATION_JSON);
-
-    //     Map<String, Object> request = new HashMap<>();
-    //     request.put("amount", dto.getAmount());
-    //     request.put("orderId", dto.getOrderId());
-    //     request.put("orderName", dto.getOrderName());
-    //     request.put("successUrl", corsOrigin + "/store/success");
-    //     // request.put("failUrl", "http://localhost:9999/store/toss/fail");
-
-    //     HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<>(request, headers);
-
-    //     String url = "https://api.tosspayments.com/v1/payments";
-
-    //     ResponseEntity<String> response = restTemplate.postForEntity(url, httpEntity, String.class);
-    //     return ResponseEntity.ok(response.getBody());
-    // }
 
     @GetMapping("/success")
     public String success(@RequestParam String paymentKey,
