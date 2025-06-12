11e5035984e9fd28b304c0be9284e28be96dc3c8
Khh-25 <kimhoonhee25@naver.com>
2025-06-12 20:49:42 +0900

    관리자 페이지report 추가
    ㅁㅇㄴㄹㄴㅇㄻㄴㅇㄻㄴㅇㄹ

--- src/main/java/com/project/spring/pawple/app/manage/ManagerController.java
+++ src/main/java/com/project/spring/pawple/app/manage/ManagerController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -451,8 +452,8 @@ public ResponseEntity<List<MonthlySalesDto>> getMonthlySales() {
     }
 
     
-
-    @GetMapping("/admin/report")
+    @ResponseBody
+    @GetMapping("/reports")
     public List<ReportEntity> getAllReports() {
         return reportRepository.findAll();
     }
